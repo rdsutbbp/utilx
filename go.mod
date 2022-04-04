7d940a4da59de988221a9f0b73d1ce0391e81c70
jaronnie <jaron@jaronnie.com>
2022-04-04 19:07:17 +0800

    fix(utilx): fix lo library

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.18
 
 require (
 	github.com/pkg/errors v0.9.1
-	github.com/samber/lo v1.6.1
+	github.com/samber/lo v1.11.0
 )
 
 require golang.org/x/exp v0.0.0-20220303212507-bbda1eaf7a17 // indirect
--- lo_test.go
+++ lo_test.go
@@ -6,9 +6,9 @@ import (
 )
 
 func TestLoUniq(t *testing.T) {
-	names := lo.Uniq[string]([]string{"Samuel", "Marc", "Samuel"})
+	names := lo.Uniq([]string{"Samuel", "Marc", "Samuel"})
 	t.Log(names)
 
-	names2 := Uniq[string]([]string{"1", "2", "1"})
+	names2 := Uniq([]string{"1", "2", "1"})
 	t.Log(names2)
 }
